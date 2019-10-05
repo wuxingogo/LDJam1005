a376d55a35683bcdc4fb79c56c7e912247431867
Wuxingogo <52111314ly@gmail.com>
2019-10-06 02:47:05 +0800

    shadersprefab

---
+++ ldjam/Assets/Scripts/BallAnimationController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallAnimationController : MonoBehaviour
+{
+    public Animator mAnimator = null;
+    public void OnPlayOpenup()
+    {
+        mAnimator.SetTrigger("tigger_to_start");
+    }
+
+    public void OnOpenupFinish()
+    {
+        XLogger.Log("On Animation Play Finish!!!");
+    }
+}
